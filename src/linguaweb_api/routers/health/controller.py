89b7e6bfdc806b67551c25fcd7caf5e3aefcda37
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-02-09 16:00:38 -0500

    Add endpoint for checking internet connectivity

--- src/linguaweb_api/routers/health/controller.py
+++ src/linguaweb_api/routers/health/controller.py
@@ -1,5 +1,18 @@
 """Controller to assess the health of the services."""
+import fastapi
+import requests
+from fastapi import status
 
 
-async def get_api_health() -> None:
+def get_api_health() -> None:
     """Returns the health of the API."""
+
+
+def get_internet_connectivity() -> None:
+    """Checks the internet connectivity of the API."""
+    response = requests.get("https://www.google.com", timeout=5)
+    if response.status_code != status.HTTP_200_OK:
+        raise fastapi.HTTPException(
+            status_code=response.status_code,
+            detail="Internet connectivity check failed.",
+        )
--- src/linguaweb_api/routers/health/views.py
+++ src/linguaweb_api/routers/health/views.py
@@ -27,4 +27,20 @@ router = fastapi.APIRouter(prefix="/health", tags=["health"])
 async def get_health() -> None:
     """Returns the health of the API."""
     logger.debug("Checking health.")
-    return await controller.get_api_health()
+    controller.get_api_health()
+
+
+@router.get(
+    "/connectivity",
+    status_code=fastapi.status.HTTP_200_OK,
+    summary="Checks the internet connectivity of the API.",
+    description=(
+        "Checks the internet connectivity of the API. This will return 200 if the "
+        "API can connect to the internet, any other response is indicative of a "
+        "serious issue."
+    ),
+)
+async def get_connectivity() -> None:
+    """Checks the internet connectivity of the API."""
+    logger.debug("Checking connectivity.")
+    controller.get_internet_connectivity()
